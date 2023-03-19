eea7dee09e080a40834f8c30ab7ec76a6f97b61b
Artur Szolke <aszolke002@gmail.com>
2023-03-19 17:35:31 +0100

    created simple navbar

---
+++ MonteCarlo/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h1>This is About Us page</h1>
+    </div>
+@endsection
---
+++ MonteCarlo/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h1>Welcome on Home Page</h1>
+    </div>
+@endsection
