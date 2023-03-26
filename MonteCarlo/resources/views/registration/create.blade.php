bd0ff53476c392a80c5afc779a615a1d450d3ffd
Dawid Zmarzlik <dawid.zmarzlik@proton.me>
2023-03-26 22:21:07 +0200

    fixed register background

--- MonteCarlo/resources/views/registration/create.blade.php
+++ MonteCarlo/resources/views/registration/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <form method="POST" action="/register">
         {{ csrf_field() }}
-        <div class="d-flex justify-content-center align-items-center height-nav">
+        <div class="d-flex justify-content-center align-items-center height-nav bg-grad">
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-sm-9 col-md-6 col-lg-4 mx-auto bg-white shadow rounded-custom p-4">
