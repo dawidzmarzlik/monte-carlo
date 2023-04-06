d8168204a12ac5d872812ffdda6c7b21dcaa1566
LukaszPietruszka <l3xuks@gmail.com>
2023-04-06 21:44:20 +0200

    Add ONLY page for single teacher

---
+++ MonteCarlo/resources/views/admin/teacherpage.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layout')
+
+@section('content')
+    <div class="m-auto">
+        <div class="row align-items-center m-auto">
+            <div class="col ps-0">
+                <h1 class="white">Instruktorzy</h1>
+            </div>
+            <div class="col text-end pe-0">
+                <a href="#" class="btn btn-add">Dodaj instruktora</a>
+            </div>
+        </div>
+        <div class="table-responsive rounded-4 mt-2">
+            
+            
+        </div>
+    </div>
+@endsection
