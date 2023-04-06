ae54df0b3cbfec8e08165f5f3d3f201c636dc263
LukaszPietruszka <l3xuks@gmail.com>
2023-04-06 19:35:28 +0200

    Add teacher list

--- MonteCarlo/resources/views/admin/teacher.blade.php
+++ MonteCarlo/resources/views/admin/teacher.blade.php
@@ -1,5 +1,44 @@
 @extends('admin.layout')
 
 @section('content')
-    <h1>Instruktorzy</h1>
+    <div class="m-auto">
+        <div class="row align-items-center m-auto">
+            <div class="col ps-0">
+                <h1 class="white">Instruktorzy</h1>
+            </div>
+            <div class="col text-end pe-0">
+                <a href="#" class="btn btn-add">Dodaj instruktora</a>
+            </div>
+        </div>
+        <div class="table-responsive rounded-4 mt-2">
+            <table class="table table-hover align-middle table-borderless admin-table m-auto">
+                <thead>
+                    <tr>
+                        <th scope="col">Imię</th>
+                        <th scope="col">Nazwisko</th>
+                        <th scope="col">Numer telefonu</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Ilość kursantów</th>
+                        <th scope="col">Pojazd</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 0; $i < 20; $i++)
+                        <tr>
+                            <td>Jan</td>
+                            <td>Kowalski</td>
+                            <td>123 456 789</td>
+                            <td>j.kowalski@gmail.com</td>
+                            <td>2</td>
+                            <td>OPO 24534</td>
+                            <td class="text-end">
+                                <a class="btn btn-table" href="#">Więcej</a>
+                            </td>
+                        </tr>
+                    @endfor
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
