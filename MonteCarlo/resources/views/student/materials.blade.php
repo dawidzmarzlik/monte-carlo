faffe2ae81f1eb31531207acf4f91e5f3030f440
LukaszPietruszka <l3xuks@gmail.com>
2023-05-21 20:29:19 +0200

    addition of educational material

--- MonteCarlo/resources/views/student/materials.blade.php
+++ MonteCarlo/resources/views/student/materials.blade.php
@@ -21,27 +21,69 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Spotkanie organizacyjne</td>
+                                <td>Wiadomości ogólne</td>
                                 <td class="text-end">
-                                    <a class="btn btn-add" href="#">Pobierz</a>
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/15TfCuvyV1BL5tzau_zLKJ8pm8ZJw5g1P/view?usp=share_link">Zobacz</a>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Przepisy o ruchu pojazdów</td>
+                                <td>Podstawowe manewry na drodze</td>
                                 <td class="text-end">
-                                    <a class="btn btn-add" href="#">Pobierz</a>
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1BDrAstTIuHigHqCEES2frvgaLPqn8BDk/view?usp=sharing">Zobacz</a>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Podstawowe pojęcia związane z ruchem drogowym</td>
+                                <td>Przecinanie się kierunków ruchu</td>
                                 <td class="text-end">
-                                    <a class="btn btn-add" href="#">Pobierz</a>
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1X5_8t_lWY7W8Ol-XPEeG7FwBDaCNc6EZ/view?usp=sharing">Zobacz</a>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Prędkość, zachowanie odstępu i hamowanie pojazdów</td>
+                                <td>Prędkość pojazdu i hamowanie</td>
                                 <td class="text-end">
-                                    <a class="btn btn-add" href="#">Pobierz</a>
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1yvlBUwmyDrYId2A2JHmq7eroSq9-VtWX/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Rodzaje znaków drogowych</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1O-I4gqmJI1sScaMEcSGcoZ65XrEPX3C7/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Uprawnienia, obowiązki i odpowiedzialność kierowcy i właściciela pojazdu</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/19nlO2rbfLGzfVntSvNZ6b8I8VmdoScgT/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Przewóz pasażerów i ładunków, jazda z przyczepą</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1dGSDlyqc-BdPH8av9xjG1ddYiftv_ONO/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Przygotowanie do jazdy</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1Ds0kE9Bok9URfPaXRCD8Q3GNW-wAObpc/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Budowa pojazdu - wiadomości ogólne</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1XAlemuguOPkEJtAk-H35uuTkgnxfLW3D/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Wpływ alkoholu, leków i narkotyków na bezpieczeństwo w ruchu drogowym</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1Rxi6Cuu50Otqs9mbNtBvT9p9fcxOr-6Y/view?usp=sharing">Zobacz</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Pierwsza pomoc</td>
+                                <td class="text-end">
+                                    <a class="btn btn-add" href="https://drive.google.com/file/d/1ruT78poySvoyYxktVclK0gachEQBno7M/view?usp=sharing">Zobacz</a>
                                 </td>
                             </tr>
                         </tbody>
